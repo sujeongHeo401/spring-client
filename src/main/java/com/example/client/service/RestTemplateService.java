c4d50babbc9519243be544ae72dc0208d547718a
sujeongHeo401 <gotnwjd40@gmail.com>
2022-03-26 10:52:41 +0900

    Initial commit

---
+++ src/main/java/com/example/client/service/RestTemplateService.java
@@ -0,0 +1,31 @@
+package com.example.client.service;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.URI;
+
+@Service
+public class RestTemplateService {
+    /// http://localhost/api/server/hello
+    // response
+    public String hello(){
+        URI uri = UriComponentsBuilder
+                .fromUriString("http://localhost:9090")
+                .path("/api/server/hello")
+                .encode()
+                .build()
+                .toUri();
+
+        System.out.println(uri.toString());
+
+        RestTemplate restTemplate = new RestTemplate();
+        ResponseEntity<String> result = restTemplate.getForEntity(uri, String.class);
+
+        System.out.println(result.getStatusCode());
+        System.out.println(result.getBody());
+
+        return result.getBody();
+    }
+}
